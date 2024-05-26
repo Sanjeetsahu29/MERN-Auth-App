a9b5d32955ef8d337e9795302a6f93b1ce3b90a4
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2024-05-27 00:09:32 +0530

    connect to the Database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app  = express();
-app.listen(3000,()=>{
-    console.log("Server is running on port:3000")
+
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected to Database")
+    app.listen(3000,()=>{
+        console.log("Server is running on port:3000")
+    })
 })
+.catch((err)=>{
+    console.log(err.message);
+})
+// after / you can add the name of your database explicitly and if not mentioned any name then by default database name will be test
+
